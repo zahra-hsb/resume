a272665c83ecdc86f59c2da7a3a7c051af77eea8
zahra-hsb <zahra16toa@gmail.com>
2024-10-21 07:10:34 -0700

    particles added to project

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
 import Details from "@/components/Details";
+import Particle from "@/components/Particles";
 import PictureContainer from "@/components/PictureContainer";
 import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
     <>
-      <section className="flex">
-        <Sidebar />
-        <PictureContainer />
-        <Details />
+      <section className="flex items-center justify-center p-10 ">
+        <div className="absolute z-0">
+          <Particle />
+        </div>
+        <section className="flex absolute top-0 shadow-custom z-50 w-[90%] backdrop-blur">
+          <Sidebar />
+          <PictureContainer />
+          <Details />
+        </section>
       </section>
     </>
   );
---
+++ src/components/Particles/index.jsx
@@ -0,0 +1,140 @@
+'use client'
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+const Particle = () => {
+    const [init, setInit] = useState(false);
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine);
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    const options = useMemo(
+        () => ({
+            background: {
+                color: {
+                    value: "",
+                },
+            },
+            fpsLimit: 1000,
+            interactivity: {
+                detect_on: "window",
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "grab",
+                    },
+                    resize: 'true'
+                },
+                modes: {
+                    push: {
+                        quantity: 4,
+                    },
+                    repulse: {
+                        distance: 100,
+                        duration: 5,
+                    },
+                    grab: {
+                        distance: 400,
+                        line_linked: {
+                            opacity: 1
+                        }
+                    },
+                },
+            },
+
+            particles: {
+                color: {
+                    value: "#ffffff",
+                },
+                links: {
+                    color: "#ffffff",
+                    distance: 250,
+                    enable: true,
+                    opacity: 0.4,
+                    width: 0.8,
+                },
+                collisions: {
+                    enable: true,
+                },
+                move: {
+                    enable: true,
+                    speed: 2,
+                    direction: "none",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                        enable: false,
+                        rotateX: 600,
+                        rotateY: 1200
+                    }
+                },
+                number: {
+                    density: {
+                        enable: true,
+                    },
+                    value: 100,
+                    value_area: 800
+                },
+                opacity: {
+                    value: 0.5,
+                },
+                shape: {
+                    type: "circle",
+                    stroke: { width: 0, color: "#000000" }
+                },
+                size: {
+                    value: 2,
+                    random: true,
+                    anim: {
+                        enable: true,
+                        speed: 10,
+                        size_min: 0.1,
+                        sync: false
+                    }
+                },
+            },
+            detectRetina: true,
+        }),
+        [],
+    );
+
+    if (init) {
+        return (
+            <Particles
+                id="tsparticles"
+                particlesLoaded={particlesLoaded}
+                options={options}
+            />
+        );
+    }
+
+    return <></>;
+};
+
+export default Particle
