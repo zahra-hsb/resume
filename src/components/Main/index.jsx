cf604416982c1747f4cfb5520dc22065d3a4e1a4
zahra-hsb <zahra16toa@gmail.com>
2025-12-12 08:21:46 -0800

    split the projects section

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,57 +1,21 @@
-"use client"
 import { useProjectContext } from "@/lib/Providers";
 import Details from "../Details";
 import Particle from "../Particles"
 import PictureContainer from "../PictureContainer";
 import Sidebar from "../Sidebar";
-import Image from "next/image";
-import { useEffect, useState } from "react";
 import ProjectBtn from "../Projects/ProjectsBtn";
-import Link from "next/link";
+import ProjectsSection from "../Sidebar/ProjectsSection";
 
 const Main = () => {
     const { isShowProjects } = useProjectContext()
-    const [workSamples, setWorkSamples] = useState([{
-        name: "",
-        github_link: "",
-        website_link: "",
-        readme_link: "",
-        readme_texts: "",
-    }])
-    useEffect(() => {
-        async function getDetails() {
-            const result = await fetch("/api/get")
-            const returnedDetails = (await result.json())
-            setWorkSamples(returnedDetails.gotData)
-        }
-        getDetails()
-        console.log(typeof workSamples[0].readme_texts)
-    }, [workSamples])
+    
     return (
         <section className="h-full flex items-center justify-center lg:p-10 realtive">
             <div className="absolute z-0">
                 <Particle />
             </div>
             {isShowProjects ?
-                <section className="bg-gray-100 bg-opacity-10 dark:bg-transparent max-w-screen-2xl mx-auto grid lg:grid-cols-2 gap-20 md:gap-10 static top-0 shadow-xl dark:shadow-custom z-50 w-[90%] backdrop-blur p-10 ">
-                    {workSamples.map((web, index) => (
-                        <>
-                            <div key={web._id} className="w-[70%] lg:w-full flex flex-col lg:flex-row items-center lg:items-start justify-evenly gap-5">
-                                {/* <Image alt="" src={} /> */}
-                                {/* <div className="w-full lg:w-1/3 h-48 rounded border-2 border-cyan-400"></div> */}
-                                <div className="w-[95%] lg:w-2/3 flex flex-col gap-5" dir="ltr">
-                                    <p>نام پروژه: {web.name}</p>
-                                    <p>لینک گیتهاب: <Link href={web.github_link} className="text-sm font-mono text-cyan-400">{"click me"}</Link></p>
-                                    <p>لینک وبسایت: <Link href={web.website_link} className="text-sm font-mono text-cyan-400">{web.website_link}</Link></p>
-                                    <div className="h-44 border rounded p-2">
-                                        <pre style={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }} dir="ltr" className="overflow-x-auto h-full">{web.readme_texts}</pre>
-                                    </div>
-                                </div>
-                            </div>
-                            {workSamples.length - 1 !== index && <hr className="!w-[75%] lg:hidden" />}
-                        </>
-                    ))}
-                </section>
+                <ProjectsSection />
                 :
                 <section className="bg-gray-100 bg-opacity-10 dark:bg-transparent max-w-screen-2xl mx-auto flex flex-col lg:flex-row absolute top-0 shadow-xl dark:shadow-custom z-50 w-[90%] backdrop-blur">
                     <Sidebar />
